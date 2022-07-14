3ff33f1c7d19d1236fda51969fa4d33fa214b70d
Alhucarr <theotugaye@gmail.com>
2022-07-14 12:37:28 +0200

    Suppression des commentaires, lissage du code et de l'affichage

--- code/telemetrie_pio/TopAeroTX/lib/TopAeroTX/TopAeroTX.h
+++ code/telemetrie_pio/TopAeroTX/lib/TopAeroTX/TopAeroTX.h
@@ -28,7 +28,7 @@
 
 #include <WireKinetis.h>
 #include <SoftwareSerial.h>
-#include <Adafruit_Sensor.h>  //librairie Adafruit générale des capteurs 
+//#include <Adafruit_Sensor.h>  //librairie Adafruit générale des capteurs 
 #include <Adafruit_BNO055.h>  //librairie du module inertiel /* Accélération */
 #include <utility/imumaths.h> //librairie pour le fonctionnement du module inertie
 #include <Adafruit_MPL3115A2.h>
@@ -42,16 +42,14 @@
 
 #define SCB_AIRCR (*(volatile uint32_t *)0xE000ED0C) // Application Interrupt and Reset Control location
 #define BNO055_SAMPLERATE_DELAY_MS (5)        /* Accélération */
-#define ALTMODE //comment out for barometer mode; default is altitude mode 
 #define mySerial Serial1      /* GPS */
-#define MAX_WAIT_FOR_TIMER 1    /* timer des fonctions */
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                        Déclarations globales                                   //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
                       /* GPS */
-
 static const int RXPin = 0, TXPin = 1;      
 static const uint32_t GPSBaud = 9600;
 SoftwareSerial ss(RXPin, TXPin);
@@ -70,24 +68,18 @@ float tab_acl[3] = {0, 0, 0};
                       /*Pression*/
 float tension1, P1, tension2, P2, tension3, P3, tension4, P4, tension5, P5, tension6, P6, tension7, P7, tension8, P8, tension9, P9, tension10, P10, tension11, P11, tension12, P12, tension13, P13, tension14, P14;//Déclaration des variables tension et pression
 
-              /* Altitude */
-
+                     /* Altitude */
 Adafruit_MPL3115A2 baro;
-/*ool first=true;
-float altm0=0.0;
-//const int SENSORADDRESS = 0x60; // address specific to the MPL3115A1, value found in datasheet 
-float altsmooth = 0; //for exponential smoothing          
-byte IICdata[5] = {0,0,0,0,0}; //buffer for sensor data 
-float firstalt=0.0;
-bool alt_must_adjust = true; //condition pour ajuster l'altitude au bout de 1min*/
 
-int comdata=0;       /* Radio */
+                       /* Radio */
+int comdata=0;       
 int data_lenght;
 String comstr;
 char* frame;
 SoftwareSerial lora(9,10);
 
-File myFile;        /* uSD */
+                        /* uSD */
+File myFile;        
 File tableau;
 const int chipSelect = BUILTIN_SDCARD;
 int i=0; //Compteur du nombre de mesures
@@ -95,13 +87,6 @@ int i=0; //Compteur du nombre de mesures
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                       Déclarations des structures                              //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-enum {EMPTY, FULL};
-
-struct mailbox_s {
-    int state; 
-    int val;
-};
 
 struct gps_s {
     String framestr; 
@@ -116,7 +101,6 @@ struct alt_s {
     int sensoraddress;
 };
 
-//struct mailbox_s    mb = {.state = EMPTY};  NO NEED MAILBOX
 struct gps_s        gps_s;
 struct time_s       time_s;
 struct alt_s        alt_s;
@@ -126,10 +110,5 @@ struct alt_s        alt_s;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void accel_read_data();
-float altitude_read_data(struct alt_s*);
-float Alt_Read();
-byte IIC_Read(byte regAddr, struct alt_s*);
-void IIC_ReadData(struct alt_s*);
-void IIC_Write(byte regAddr, byte value, struct alt_s*);
 void printcsv(File tab, float val);
 void printcsv(File tab, String val); //Surcharge
--- code/telemetrie_pio/TopAeroTX/src/TopAeroTX.cpp
+++ code/telemetrie_pio/TopAeroTX/src/TopAeroTX.cpp
@@ -70,16 +70,6 @@ void setup_alt(struct alt_s* alt_s){
     Serial.println("Couldnt find altimeter sensor");
     while(1);
   }
-  
-  /*alt_s->sensoraddress= 0x60; // address specific to the MPL3115A1, value found in datasheet 
-  IIC_Write(0x2D,0, alt_s); //write altitude offset=0 (because calculation below is based on offset=0)
-  //Altitude mode
-  IIC_Write(0x26, 0b10111011, alt_s); //bit 2 is one shot mode //0xB9 = 0b10111001
-  IIC_Write(0x26, 0b10111001, alt_s); //must clear oversampling (OST) bit, otherwise update will be once per second
-  delay(1000); //wait for measurement
-  IIC_ReadData(alt_s); //
-  firstalt=Alt_Read();
-  altsmooth=Alt_Read()-firstalt;*/
 }
 
                                                 /* Radio */ 
@@ -133,76 +123,76 @@ void setup_uSD(){
 void loop_pression(){
   tension2 = analogRead(A16) * 5.0 / 1023.0;
   P2 = (tension2 / 0.002 - 39.75);
-  Serial.println("P2 :");
-  Serial.print(P2, 0);
-  Serial.println(" hPa");
+  //Serial.println("P2 :");
+  //Serial.print(P2, 0);
+  //Serial.println(" hPa");
   
   tension4 = analogRead(A18) * 5.0 / 1023.0;
   P4 = (tension4 / 0.002 - 34.75);
-  Serial.println("P4 :");
-  Serial.print(P4, 0);
-  Serial.println(" hPa");
+  //Serial.println("P4 :");
+  //Serial.print(P4, 0);
+  //Serial.println(" hPa");
   
   tension5 = analogRead(A14) * 5.0 / 1023.0;
   P5 = (tension5 / 0.002 - 48.75);
-  Serial.println("P5 :");
-  Serial.print(P5, 0);
-  Serial.println(" hPa");
+  //Serial.println("P5 :");
+  //Serial.print(P5, 0);
+  //Serial.println(" hPa");
   
   tension6 = analogRead(A15) * 5.0 / 1023.0;
   P6 = (tension6 / 0.002 - 44.75);
-  Serial.println("P6 :");
-  Serial.print(P6, 0);
-  Serial.println(" hPa");
+  //Serial.println("P6 :");
+  //Serial.print(P6, 0);
+  //Serial.println(" hPa");
   
   tension7 = analogRead(A17) * 5.0 / 1023.0;
   P7 = (tension7 / 0.002 - 39.75);
-  Serial.println("P7 :");
-  Serial.print(P7, 0);
-  Serial.println(" hPa");
+  //Serial.println("P7 :");
+  //Serial.print(P7, 0);
+  //Serial.println(" hPa");
   
   tension8 = analogRead(A9) * 5.0 / 1023.0;
   P8 = (tension8 / 0.002 - 53.75);
-  Serial.println("P8 :");
-  Serial.print(P8, 0);
-  Serial.println(" hPa");
+  //Serial.println("P8 :");
+  //Serial.print(P8, 0);
+  //Serial.println(" hPa");
   
   tension9 = analogRead(A8) * 5.0 / 1023.0;
   P9 = (tension9 / 0.002 - 24.75);
-  Serial.println("P9 :");
-  Serial.print(P9, 0);
-  Serial.println(" hPa");
+  //Serial.println("P9 :");
+  //Serial.print(P9, 0);
+  //Serial.println(" hPa");
   
   tension10 = analogRead(A7) * 5.0 / 1023.0;
   P10 = (tension10 / 0.002 - 29.75);
-  Serial.println("P10 :");
-  Serial.print(P10, 0);
-  Serial.println(" hPa");
+  //Serial.println("P10 :");
+  //Serial.print(P10, 0);
+  //Serial.println(" hPa");
   
   tension11 = analogRead(A6) * 5.0 / 1023.0;
   P11 = (tension11 / 0.002 - 31.75);
-  Serial.println("P11 :");
-  Serial.print(P11, 0);
-  Serial.println(" hPa");
+  //Serial.println("P11 :");
+  //Serial.print(P11, 0);
+  //Serial.println(" hPa");
   
   tension12 = analogRead(A3) * 5.0 / 1023.0;
   P12 = (tension12 / 0.002 - 36.75);
-  Serial.println("P12 :");
-  Serial.print(P12, 0);
-  Serial.println(" hPa");
+  //Serial.println("P12 :");
+  //Serial.print(P12, 0);
+  //Serial.println(" hPa");
 
   tension13 = analogRead(A2) * 5.0 / 1023.0;
   P13 = (tension13 / 0.002 - 53.75);
-  Serial.println("P13 :");
-  Serial.print(P13, 0);
-  Serial.println(" hPa");
+  //Serial.println("P13 :");
+  //Serial.print(P13, 0);
+  //Serial.println(" hPa");
 
   tension14 = analogRead(A1) * 5.0 / 1023.0;//Lecture de la tension et conversion en V
   P14 = (tension14 / 0.002 - 49.75); //Obtention de la pression en hPa => valeur 93 = offset , a changer pour chaque capteur 
   //Affichage de la pression sur le3moniteur série
-  Serial.println("P14 :");
-  Serial.print(P14, 0);
-  Serial.println(" hPa");
+  //Serial.println("P14 :");
+  //Serial.print(P14, 0);
+  //Serial.println(" hPa");
 }
 
                                                     /* GPS */
@@ -236,25 +226,12 @@ void loop_led(struct time_s* time_s){
 
                                                 /* Accélération (m/s^2) */
 void loop_accel(){
-  // mib note : plus besoin a compter du 2021-07-20.
-  //Serial.print(accelModule);
-  //Serial.println("");
   accel_read_data();
 }
 
                                                   /* Altitude (m) */ 
 void loop_alt(struct time_s* time_s, struct alt_s* alt_s){
   alt_s->altitudeSmoothed=baro.getAltitude();
-  /*alt_s->altitudeSmoothed=altitude_read_data(alt_s)-altm0;//soustraire altm0 pour commencer à l'altitude 0
-  if(first==true){                            // +/- 1m de précision
-    if(time_s->ms>60000){ //on attend 1min pour avoir une valeur stable d'altitude
-      altm0=alt_s->altitudeSmoothed;
-      first=false;
-    }
-  }
-  
-  Serial.print(time_s->ms);
-  Serial.println("");*/
 }
 
                                                       /* Radio */
@@ -267,8 +244,7 @@ void loop_radio(struct gps_s* gps_s, struct alt_s* alt_s){
   while(lora.available()){
     Serial.write(lora.read());
   }
-  Serial.println();
-  Serial.println(cmd);
+  Serial.print(cmd);
   delay(35); //le délai influe sur la transmission radio, si trop court il y a une probabilité +/- importante 
   //que les messages soient coupés
 }
@@ -346,91 +322,10 @@ void accel_read_data(){
   /* Vecteur contenant les composantes des accélérations */
   imu::Vector<3> accelerometer = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
 
-  /*float mod_accel=sqrt(pow(accelerometer.x(),2)+pow(accelerometer.y(),2)+pow(accelerometer.z(),2))-9.80665;
-  if (mod_accel<0){
-   mod_accel=0;
-  }*/
-  /*
-  Serial.print("Acceleration:");
-  Serial.println(accelerometer.x());
-  Serial.println(accelerometer.y());
-  Serial.println(accelerometer.z());
-  delay(BNO055_SAMPLERATE_DELAY_MS);*/
-
   tab_acl[0] = accelerometer.x();
   tab_acl[1] = accelerometer.y();
   tab_acl[2] = accelerometer.z();
 }
-
-                                                    /* Altitude */
-/*
-float altitude_read_data(struct alt_s* alt_s){
-                                        //by Henry Lahr, 2013-02-27, libre de droit
-  // This function reads the altitude (or barometer) and temperature registers, then prints their values
-  // variables for the calculations
-  float altbaro;
-  //One shot mode at 0b10101011 is slightly too fast, but better than wasting sensor cycles that increase precision
-  //one reading seems to take 4ms (datasheet p.33);
-  //oversampling at 32x=130ms interval between readings seems to be optimal for 10Hz
-  //#ifdef ALTMODE //Altitude mode
-    IIC_Write(0x26, 0b10111011, alt_s); //bit 2 is one shot mode //0xB9 = 0b10111001
-    IIC_Write(0x26, 0b10111001, alt_s); //must clear oversampling (OST) bit, otherwise update will be once per second
-  //#else //Barometer mode
-    //IIC_Write(0x26, 0b00111011); //bit 2 is one shot mode //0xB9 = 0b10111001
-    //IIC_Write(0x26, 0b00111001); //must clear oversampling (OST) bit, otherwise update will be once per second
-  //#endif
-  //delay(20); //read with 10Hz; drop this if calling from an outer loop
-  IIC_ReadData(alt_s); //reads registers from the sensor
-  #ifdef ALTMODE //converts byte data into float; change function to Alt_Read() or Baro_Read()
-    altbaro = Alt_Read()-firstalt;
-  #endif
-  //altsmooth=(altsmooth*3+altbaro)/4; //exponential smoothing to get a smooth time series, pas bon pour des changements brutaux
-  //Serial.print(altbaro); // in meters 
-  //Serial.print(",");
-  //Serial.print(altsmooth); // exponentially smoothed
-  //Serial.println("");
-  Serial.print("Altitude:");
-  Serial.print(altbaro); //en m
-  Serial.println("");
-  return(altbaro-0.3);//+0,3 pour compenser la dérive du capteur dû au one shot mode
-}
-
-float Alt_Read(){
-  //Reads altitude data (if CTRL_REG1 is set to altitude mode)
-  int m_altitude = IICdata[0];
-  int c_altitude = IICdata[1];
-  float l_altitude = (float)(IICdata[2]>>4)/16;
-  return((float)((m_altitude << 8)|c_altitude) + l_altitude);
-}
- 
-byte IIC_Read(byte regAddr, struct alt_s* alt_s){
-  // This function reads one byte over I2C
-  Wire.beginTransmission(alt_s->sensoraddress);
-  Wire.write(regAddr); // Address of CTRL_REG1
-  Wire.endTransmission(false); // Send data to I2C dev with option for a repeated start. Works in Arduino V1.0.1
-  Wire.requestFrom(alt_s->sensoraddress, 1);
-  return Wire.read();
-}
- 
-void IIC_ReadData(struct alt_s* alt_s){  //Read Altitude/Barometer and Temperature data (5 bytes)
-  //This is faster than reading individual register, as the sensor automatically increments the register address,
-  //so we just keep reading...
-  byte i=0;
-  Wire.beginTransmission(alt_s->sensoraddress);
-  Wire.write(0x01); // Address of CTRL_REG1
-  Wire.endTransmission(false);
-  Wire.requestFrom(alt_s->sensoraddress,5); //read 5 bytes: 3 for altitude or pressure, 2 for temperature
-  while(Wire.available()) IICdata[i++] = Wire.read();
-}
- 
-void IIC_Write(byte regAddr, byte value, struct alt_s* alt_s){
-  // This function writes one byto over I2C
-  Wire.beginTransmission(alt_s->sensoraddress);
-  Wire.write(regAddr);
-  Wire.write(value);
-  Wire.endTransmission(true);
-}
-*/
                                                     /* lisibilité d'écriture */
 void printcsv(File tab, float val){
   tab.print(val);
